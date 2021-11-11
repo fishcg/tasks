dcae1cc92647f14d3f740cc11ba1385eddefb18a
jiepeng <jiepengthegreat@126.com>
2021-11-11 20:13:36 +0800

    add config

--- verfity_notice.js
+++ verfity_notice.js
@@ -1,5 +1,7 @@
 const got = require('got')
 const schedule = require('node-schedule') // 参考文档：https://www.jianshu.com/p/8d303ff8fdeb
+const plan = require('./plan')
+var _ = require('lodash');
 
 /**
  *
@@ -8,7 +10,8 @@ const schedule = require('node-schedule') // 参考文档：https://www.jianshu.
  * @param mentionedMobileList 要 @ 成员的手机号数组
  * @return {Promise<void>}
  */
-const sendMessage = async (key, message, mentionedMobileList) => {
+const sendMessage = async (key, messages, mentionedMobileList) => {
+  let message = _.sample(messages)
   let resp = await got.post(`https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=${key}`, {
     json: {
       msgtype: 'text',
@@ -22,36 +25,10 @@ const sendMessage = async (key, message, mentionedMobileList) => {
   console.log(resp.body.errmsg)
 }
 
-/**
- * 定点吃维生素
- */
-const vitamin = (key, message, mentionedMobileList)=>{
-  //每分钟的第30秒定时执行一次:
-  schedule.scheduleJob('0 10 14 * * *', async () => {
-    sendMessage(key, message, mentionedMobileList)
-  })
+for (const [key, plans] of Object.entries(plan)) {
+  for (const plan of plans) {
+    schedule.scheduleJob(plan.rule, async () => {
+      sendMessage(key, plan.msgs, plan.users)
+    })
+  }
 }
-
-/**
- * 定点吃饭
- */
-const haveDinner = (key, message, mentionedMobileList)=>{
-  //每分钟的第30秒定时执行一次:
-  schedule.scheduleJob('0 0 18 * * *', async () => {
-    sendMessage(key, message, mentionedMobileList)
-  })
-}
-
-/**
- * 定时喝水
- */
-const drink = (key, message, mentionedMobileList)=>{
-  //每分钟的第30秒定时执行一次:
-  schedule.scheduleJob('0 0 11-20 * * 1-5', async () => {
-    sendMessage(key, message, mentionedMobileList)
-  })
-}
-
-vitamin('', '吃维生素啦~', ['15685598480', '18800160926', '18770913818'])
-haveDinner('', '工作一天了，吃点东西吧~', [])
-drink('', '天干物燥，多喝热水~', [])
